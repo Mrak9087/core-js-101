f8b526a89dd8444684c4c6ffc12511cd7d3be1f7
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2020-12-07 19:55:24 +0400

    feat: implements numbers

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -52,7 +52,7 @@ function getCicleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-  return (value1 + value2) / 2;
+  return value1 / 2 + value2 / 2;
 }
 
 /**
@@ -109,8 +109,8 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  return Math.acos((x1 * x2 + y1 * y2) / ((Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2))));
 }
 
 /**
@@ -201,7 +201,13 @@ function roundToPowerOfTen(num, pow) {
  *   17 => true
  */
 function isPrime(n) {
-  return n % 2 !== 0;
+  const max = Math.floor(Math.sqrt(n));
+  for (let i = 2; i <= max; i += 1) {
+    if ((n % i) === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
